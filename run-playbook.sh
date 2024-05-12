6605f98f61c5fa93807f6e1d21688897facd9e78
bagussa <emailbagussa@gmail.com>
2024-05-12 23:03:28 +0700

    add playbook and host dns bind9

--- run-playbook.sh
+++ run-playbook.sh
@@ -1,2 +1,4 @@
 sudo apt-get install ansible -y
 ansible-playbook mail-bagussa-my-id-playbook.yml --ask-become-pass -i hosts
+ansible-playbook dns-sabang-net-playbook.yml --ask-become-pass -i hosts
+
