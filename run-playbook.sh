d9da1e315a09d9d8b465cd1a0ddfe6caccb02ddc
bagussa <emailbagussa@gmail.com>
2024-05-25 08:03:43 +0700

    nginx role

--- run-playbook.sh
+++ run-playbook.sh
@@ -1,4 +1,5 @@
 sudo apt-get install ansible -y
 ansible-playbook mail-bagussa-my-id-playbook.yml --ask-become-pass -i hosts
 ansible-playbook dns-sabang-net-playbook.yml --ask-become-pass -i hosts
+ansible-playbook www-sabang-net-playbook.yml --ask-become-pass -i hosts
 
